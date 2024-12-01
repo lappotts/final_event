f529410bfda10b8e00d9537f41f1b3e5f542be98
usigboka <usigboka@cs.olemiss.edu>
2024-12-01 17:13:16 -0600

    Made a few changes to my files

--- app/about_us/page.tsx
+++ app/about_us/page.tsx
@@ -8,22 +8,20 @@ import Footer from "../components/Footer";
 import { List } from "@mui/material";
 // import "../styles.css";
 
-
 // function to display "About Us" information
-export default function RegisterPage() {
-    return (
-
+export default function AboutUsPage() {
+    return (        
     <div style={{ padding: "20px" }}>
-    <Header />
+    <Header/>
     <h3 className="text-2xl font-bold m-5">About Us</h3>
     <h4 className="m-5"><strong>Meta-04</strong></h4>
-    <h2 className="m-5">Description</h2><br/>
+    <h2 className="m-5">We are a team of four CS students. This is our CS 387 class project.</h2><br/>
     <h5 className="m-5"><strong>Team Members</strong></h5>
     <ul>
-        <li className="m-5"><img></img>Sushmita Halder</li>
-        <li className="m-5"><img></img>Stephen Igboka</li>
-        <li className="m-5"><img ></img>Jacob Miller</li>
-        <li className="m-5"><img ></img>Layton Potts</li>
+        <li className="m-5">- Sushmita Halder</li>
+        <li className="m-5">- Stephen Igboka</li>
+        <li className="m-5">- Jacob Miller</li>
+        <li className="m-5">- Layton Potts</li>
     </ul>    
     </div>
     );
--- app/terms_of_service/page.tsx
+++ app/terms_of_service/page.tsx
@@ -1,10 +1,11 @@
- // app/terms_of_service/page.tsx
+// app/terms_of_service/page.tsx
 "use client";
 
 // import statements
 import React, { useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Link from "next/link";
 
 // function to display terms of service
 export default function terms_of_service() {
@@ -12,7 +13,7 @@ export default function terms_of_service() {
 <div style={{ padding: "20px" }}>
       <Header />
       <h3 className="text-2xl font-bold m-5">Event Center Terms of Service</h3>
-      <h5>Click here to see our terms of service document</h5>
+      <h5 className="m-5"> <Link href=""><u>Click here to see our Terms of Service document</u></Link></h5>
     </div>
     );
 }
